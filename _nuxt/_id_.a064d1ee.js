b4f9a52d9c3dd9b3b59811a37989471e31efd2cf
anythingelse428 <vladn803@gmail.com>
2023-11-30 13:42:23 +0400

    Updates

--- _nuxt/_id_.a064d1ee.js
+++
@@ -1,1 +0,0 @@
-import{n as t,u as o,o as n,c as s,t as a,q as r}from"./entry.a6fbf934.js";const l=t({__name:"[id]",setup(c){const e=o().params.id;return(i,p)=>(n(),s("div",null,a(r(e)),1))}});export{l as default};
