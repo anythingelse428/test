10909246a2dcb80a1cf96b4978ebf813ddd7bc75
anythingelse428 <vladn803@gmail.com>
2023-12-04 16:16:20 +0400

    1

---
+++ assets/support.a0235baa.js
@@ -0,0 +1,1 @@
+import"./entry.e047550e.js";const t=""+new URL("backend.536758fe.svg",import.meta.url).href,e=""+new URL("support.1ae0d16f.svg",import.meta.url).href;export{t as _,e as a};
--- assets/ui.f334ebe1.js
+++
@@ -1,1 +0,0 @@
-import"./entry.7922e4bd.js";const t=""+new URL("ui.a5fff18c.svg",import.meta.url).href;export{t as _};
