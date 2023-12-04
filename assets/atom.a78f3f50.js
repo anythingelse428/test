1d35a36ac86aa65c580a5357995764b27239e990
anythingelse428 <vladn803@gmail.com>
2023-12-04 18:36:33 +0400

    1

--- assets/atom.a78f3f50.js
+++
@@ -1,1 +0,0 @@
-import"./entry.c9c942a1.js";const o=""+new URL("atom.9f4cc984.svg",import.meta.url).href;export{o as _};
--- assets/ui.3d4a93b5.js
+++
@@ -1,1 +0,0 @@
-import"./entry.c9c942a1.js";const t=""+new URL("ui.a5fff18c.svg",import.meta.url).href;export{t as _};
