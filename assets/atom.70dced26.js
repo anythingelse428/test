dece019fe874a6318c30eae9ddb9d2196030ab72
anythingelse428 <vladn803@gmail.com>
2023-12-04 18:45:47 +0400

    2

--- assets/atom.70dced26.js
+++
@@ -1,1 +0,0 @@
-import"./entry.bb9aa8c9.js";const o=""+new URL("atom.9f4cc984.svg",import.meta.url).href;export{o as _};
--- assets/diagramm.4d2a348e.js
+++
@@ -1,1 +0,0 @@
-import"./entry.bb9aa8c9.js";const r=""+new URL("pen.7feaeb93.svg",import.meta.url).href,t=""+new URL("diagramm.9998d36e.svg",import.meta.url).href;export{r as _,t as a};
