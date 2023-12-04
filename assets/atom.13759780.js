afe3826aa170d47aafc3e1e2c1909f241e5c4463
anythingelse428 <vladn803@gmail.com>
2023-12-04 19:14:42 +0400

    1

--- assets/atom.13759780.js
+++
@@ -1,1 +0,0 @@
-import"./entry.d100812a.js";const o=""+new URL("atom.9f4cc984.svg",import.meta.url).href;export{o as _};
--- assets/ui.3f13782f.js
+++
@@ -1,1 +0,0 @@
-import"./entry.d100812a.js";const t=""+new URL("ui.a5fff18c.svg",import.meta.url).href;export{t as _};
